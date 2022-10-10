502f73ed87a86143a74dcfe2ad0aecfbde892808
Gustavo Kazuo <73189635+kazuohagy@users.noreply.github.com>
2022-10-09 22:57:15 -0300

    :bug: fix warning in script

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 // FAZER UM .ENV DEPOIS nao e recomendado deixar keys no
 //a3716f3bb1655185feee051b1e9ea397 key
+// m35PZP1GOTMIowNPX6Rk70Hf5WdKJlbhZSRtk0KNndk
+// https://api.unsplash.com/search/photos/?query=mario&client_id=m35PZP1GOTMIowNPX6Rk70Hf5WdKJlbhZSRtk0KNndk
 const apiKey= "a3716f3bb1655185feee051b1e9ea397";
 const apiCountryURL = "https://countryflagsapi.com/png/"
 
@@ -14,18 +16,26 @@ const countryElement = document.querySelector("#country")
 const umidityElement = document.querySelector("#umidity span")
 const windElement = document.querySelector("#wind span")
 const weatherContainer = document.querySelector("#weather-data")
+const warningElement = document.querySelector("#warning")
+
 // Funcoes
 const getWeatherData = async(city)=>{
     const apiWeatherURL =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
-
     const res = await fetch(apiWeatherURL)
     const data = await res.json();
-    console.log("Esse aqui e a data", data)
+    // console.log("Esse aqui e a data", data)
+    return data;
+}
+const getCountries = async(city)=>{
+    const apiCountryURL = `https://api.unsplash.com/search/photos/?query=${city}&orientation=landscape&client_id=m35PZP1GOTMIowNPX6Rk70Hf5WdKJlbhZSRtk0KNndk`
+    const res = await fetch(apiCountryURL)
+    const data = await res.json();
+    console.log("Esse aqui e a Cidade", data)
     return data;
 }
 const showWeatherData = async(city)=>{
+    try{
     const data = await getWeatherData(city);
-
     cityElement.innerText = data.name;
     tempElement.innerText = data.main.temp;
     descElement.innerText = data.weather[0].description;
@@ -34,17 +44,30 @@ const showWeatherData = async(city)=>{
     umidityElement.innerText = data.main.humidity+"%";
     windElement.innerText = data.wind.speed + "km/h"
     weatherContainer.classList.remove("hide")
+    warningElement.classList.add("hide")
+    }catch(err){
+        weatherContainer.classList.add("hide")
+        showError("Nao foi possivel encontrar o nome desta cidade")
+    }
 }
-
+const showError = (err)=>{
+    warningElement.classList.remove("hide")
+    warningElement.innerHTML = err;
+}
 // Eventos
 searchBtn.addEventListener("click",(e)=>{
     e.preventDefault()
     const city = cityInput.value;
-    showWeatherData(city)
+        showWeatherData(city)
+        getCountries(city)
+
+    
 })
 
 cityInput.addEventListener("keyup", (e)=>
 {if(e.code == "Enter"){
     const city = e.target.value;
     showWeatherData(city)
+    getCountries(city)
+
 }})
